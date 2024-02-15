ce0192e398295336120b6a6857125e42ff23dc62
Sebastián Carmona Ángel <68665717+Sebasc322@users.noreply.github.com>
2024-02-15 14:19:06 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -24,22 +24,18 @@ st.write("If you want to know more about the project or me, please visit the Git
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    #st.markdown("[![Linkedin](www.linkedin.com/in/sebascarmona)](linkedin.png)")
-    st.image('https://drive.google.com/uc?export=view&id=1M_u1fZsSSCWwu88S4ts8ynw_bFDtB2iX', caption='', width=70)
+    st.image('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbeaconman.com%2Fwp-content%2Fuploads%2F2020%2F11%2FLinkedin_symbol_transparent.png&f=1&nofb=1&ipt=4796ff1975a7a198f5c6dc48c204d6fcffa773a0c3a269d3fb960d9ea94323bd&ipo=images', caption='', width=70)
     st.markdown(f"[Linkedin](https://www.linkedin.com/in/sebascarmona)", unsafe_allow_html=True)
 
 
 with col2:
-    #st.markdown("[![Linkedin](www.linkedin.com/in/sebascarmona)](linkedin.png)")
-    st.image('https://drive.google.com/uc?export=view&id=14Ks568aUeZbkRlQoHdC0qL4ZUVHJl4lx', caption='', width=70)
+    st.image('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F11%2FGitHub-Symbol.png&f=1&nofb=1&ipt=65787a08ff9d6b6c471d1a8e2d9bc51c01d28950686c6726bb7166105903548d&ipo=images', caption='', width=70)
     st.markdown(f"[Github](https://github.com/Sebasc322)", unsafe_allow_html=True)
 
 with col3:
-    #st.markdown("[![Linkedin](www.linkedin.com/in/sebascarmona)](linkedin.png)")
-    st.image('https://drive.google.com/uc?export=view&id=1QKyL5SulGWYfuqcmDN1JvsNgVeSJM04-', caption='', width=70)
+    st.image('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.underconsideration.com%2Fbrandnew%2Farchives%2Fmedium_2017_monogram.png&f=1&nofb=1&ipt=966d53db79baf267aa2763fef5d0a01aca415869c243101633d1ca6223c1006f&ipo=images', caption='', width=70)
     st.markdown(f"[Medium](https://sebascar322.medium.com/)", unsafe_allow_html=True)
 
 with col4:
-    #st.markdown("[![Linkedin](www.linkedin.com/in/sebascarmona)](linkedin.png)")
-    st.image('https://drive.google.com/uc?export=view&id=1STGtVysd1tFS1rMeTyQYvacQ2cmX1Qmn', caption='', width=70)
+    st.image('https://lh3.googleusercontent.com/Pec5rs7UNUCoFM-jD79t7AMvAhOykep7aMTU3NvicangR9zsJykjMgjwFCUtU_DQMcovP_u-cggQYqJ55Kp9Z-xXMQjsNChpP5npUEEL7hFveWAj6JOVZQTR23mVthzHIA=w1280', caption='', width=70)
     st.markdown(f"[Portfolio](https://www.sebascarmona.com/)", unsafe_allow_html=True)
