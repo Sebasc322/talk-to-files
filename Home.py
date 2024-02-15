5bc8240b74562efa9e221e28d1700a8875c56109
Sebastián Carmona Ángel <68665717+Sebasc322@users.noreply.github.com>
2024-02-15 15:51:23 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Home", page_icon="🏡", layout="wide")
 #icon_path = 'https://lh5.googleusercontent.com/dovsuxe_56wOzr-ZCWqJAakP8K4GYl73alTFb_NTAr7UjwUNUdjK54cRoc8lAecqic5AiJF98NTXdlRd8V_kWko=w16383'
 image_path = Path("/mount/src/talk-to-files/srcs/logo.png").with_name("logo.png")
 st.write(image_path)
-#st.sidebar.image(str(image_path), width=170)
+st.sidebar.image(str(image_path), width=170)
 
 st.title('Talk to your Files! 🚀')
 
